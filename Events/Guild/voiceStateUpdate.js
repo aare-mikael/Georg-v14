8154153ab593f142d53618bad0c88c399010e552
Mikael Aare <aaremikael@gmail.com>
2023-06-11 00:56:16 +0200

    Connected to MongoDB

--- Events/Guild/voiceStateUpdate.js
+++ Events/Guild/voiceStateUpdate.js
@@ -1,9 +1,5 @@
 const { GuildMember, Embed, InteractionCollector } = require("discord.js");
-const {
-  joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
-} = require("@discordjs/voice");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
 const mongoose = require("mongoose");
 const mongodb = require("../../config.json").mongodb;
 const discordUsers = require("../../mongoSchemas/discordUsers");
@@ -16,50 +12,43 @@ module.exports = {
     // Georg_v14
     if (newUser.id == 1089200999933169714) return;
 
-    // Georg
+    // Georg_v12
     if (newUser.id == 741703921877123164) return;
-
-    const resource = createAudioResource(
-      "https://www.myinstants.com/media/sounds/lets-go_fIvYyAr.mp3",
-      {
-        inlineVolume: true,
-      }
-    );
-    const connection = joinVoiceChannel({
-      channelId: newUser.channel.id,
-      guildId: newUser.channel.guild.id,
-      adapterCreator: newUser.channel.guild.voiceAdapterCreator,
-    });
-
-    const player = createAudioPlayer();
-    connection.subscribe(player);
-    player.play(resource);
-    /*
-    await mongoose
-      .connect(mongodb, {
+    
+    if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(mongodb, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-      })
-      .then(async (mongoose) => {
+        });
+    }
         try {
-          const result = await discordUsers.findOne(
-            {
-              id: newUser.id,
-            },
-            {
-              user: newUser.user,
-              introSound: link,
-            },
-            {
-              upsert: true,
-              new: true,
+            const user = await discordUsers.findOne({id: newUser.id});
+            if (user) {
+              const introSound = user.introSound;
+              if (introSound) {
+              const resource = createAudioResource(introSound, { inlineVolume: true });
+              const connection = joinVoiceChannel({
+                channelId: newUser.channel.id,
+                guildId: newUser.channel.guild.id,
+                adapterCreator: newUser.channel.guild.voiceAdapterCreator,
+              });
+      
+              const player = createAudioPlayer();
+              connection.subscribe(player);
+              player.play(resource);
+
+
+              setInterval(console.log("Waiting 5 seconds before checking if player is Idle"), 5000);
+              player.on(AudioPlayerStatus.Idle, () => {
+                player.stop();
+            });
             }
-          );
-        } finally {
-          mongoose.connection.close();
-        }
-      });*/
+            }
+        } catch (err) {
+            console.log(err);
+            return;
+        } 
     return;
   },
 };
