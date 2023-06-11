aaac0d4fcbecb763147eec8c465ab76a27761bc6
Mikael Aare <aaremikael@gmail.com>
2023-06-11 15:41:05 +0200

    moved previous logic

--- Events/Guild/voiceStateUpdate.js
+++ Events/Guild/voiceStateUpdate.js
@@ -37,26 +37,28 @@ module.exports = {
           const player = createAudioPlayer();
           connection.subscribe(player)
           player.play(resource)
+
+
+          connection.on(VoiceConnectionStatus.Disconnected, async (oldUser, newUser) => {
+            try {
+              await Promise.race([
+                entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+              ]);
+              // Seems to be reconnecting to a new channel - ignore disconnect
+            } catch (error) {
+              // Seems to be a real disconnect which SHOULDN'T be recovered from
+              connection.destroy();
+            }
+          });
+
+
         }
       }
     } catch (err) {
       console.log(err);
       return;
     }
-
-    connection.on(VoiceConnectionStatus.Disconnected, async (oldUser, newUser) => {
-      try {
-        await Promise.race([
-          entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-          entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-        ]);
-        // Seems to be reconnecting to a new channel - ignore disconnect
-      } catch (error) {
-        // Seems to be a real disconnect which SHOULDN'T be recovered from
-        connection.destroy();
-      }
-    });
-
     return;
   },
 };
