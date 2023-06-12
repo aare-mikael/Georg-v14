bd427894e6abede4fea6d93ec7f7802740fbf915
Mikael Aare <aaremikael@gmail.com>
2023-06-12 14:42:09 +0200

    Added more events

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -16,10 +16,10 @@ module.exports = {
             console.log('[+]'.green + ' MongoDB connection succesful.')
         }
 
-        const activities = ["you", "Netflix", "Gameflow", "PST activities"];
+        const activities = ["you", "Netflix", "your browsing history", "your status", "Gameflow", "your gameplay", "PST activities", "your commands", "Twitch", "your voice channel", "your messages", "YouTube", "your DMs", "your profile", "your permissions"];
         let i = 0;
 
-        setInterval(() => client.user.setPresence({ activities: [{ name: activities[i++ % activities.length], type: ActivityType.Watching }] }), 15000);
+        setInterval(() => client.user.setPresence({ activities: [{ name: activities[i++ % activities.length], type: ActivityType.Watching }] }), 30000);
         console.log(`[ONLINE]`.green + ` ${client.user.tag} is online in ${client.guilds.cache.size} servers! `);
     },
 };
