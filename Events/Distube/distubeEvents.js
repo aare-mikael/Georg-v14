4d4e3c6608ca50cdc9928a08dac00699abc78830
Mikael Aare <aaremikael@gmail.com>
2023-06-12 14:19:11 +0200

    hotfix

--- Events/Distube/distubeEvents.js
+++ Events/Distube/distubeEvents.js
@@ -1,59 +1,54 @@
 const client = require("../../index.js");
 const { EmbedBuilder } = require("discord.js");
 
-
 const status = queue =>
-        `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
-        }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
-
-// if the reason this class is used is because a user joins a voice channel, this will return true
-const voiceStateUpdate = (!queue.textChannel)
-
-if (voiceStateUpdate) {
-    return;
-} else {
-    client.distube
-        .on('playSong', (queue, song) =>
-            queue.textChannel.send({
-                embeds: [new EmbedBuilder().setColor("Green")
-                    .setDescription(`🎶 | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user
-                        }\n${status(queue)}`)]
-            })
-        )
-        .on('addSong', (queue, song) =>
-            queue.textChannel.send(
-                {
-                    embeds: [new EmbedBuilder().setColor("Green")
-                        .setDescription(`🎶 | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`)]
-                }
-            )
-        )
-        .on('addList', (queue, playlist) =>
-            queue.textChannel.send(
-                {
-                    embeds: [new EmbedBuilder().setColor("Green")
-                        .setDescription(`🎶 | Added \`${playlist.name}\` playlist (${playlist.songs.length
-                            } songs) to queue\n${status(queue)}`)]
-                }
-            )
-        )
-        .on('error', (channel, e) => {
-            if (channel) channel.send(`⛔ | An error encountered: ${e.toString().slice(0, 1974)}`)
-            else console.error(e)
-        })
-        .on('empty', channel => channel.send({
-            embeds: [new EmbedBuilder().setColor("Red")
-                .setDescription('⛔ |Voice channel is empty! Leaving the channel...')]
-        }))
-        .on('searchNoResult', (message, query) =>
-            message.channel.send(
-                {
-                    embeds: [new EmbedBuilder().setColor("Red")
-                        .setDescription('`⛔ | No result found for \`${query}\`!`')]
-                })
-        )
-        .on('finish', queue => queue.textChannel.send({
+    `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+    }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+    
+    // This will be true if the voiceStateUpdate event is triggered instead of if the play-command was used.
+    if (!queue.textChannel) return;
+    
+client.distube
+    .on('playSong', (queue, song) =>
+        queue.textChannel.send({
             embeds: [new EmbedBuilder().setColor("Green")
-                .setDescription('🏁 | Queue finished!')]
-        }))
-}
+                .setDescription(`🎶 | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user
+                    }\n${status(queue)}`)]
+        })
+    )
+    .on('addSong', (queue, song) =>
+        queue.textChannel.send(
+            {
+                embeds: [new EmbedBuilder().setColor("Green")
+                    .setDescription(`🎶 | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`)]
+            }
+        )
+    )
+    .on('addList', (queue, playlist) =>
+        queue.textChannel.send(
+            {
+                embeds: [new EmbedBuilder().setColor("Green")
+                    .setDescription(`🎶 | Added \`${playlist.name}\` playlist (${playlist.songs.length
+                        } songs) to queue\n${status(queue)}`)]
+            }
+        )
+    )
+    .on('error', (channel, e) => {
+        if (channel) channel.send(`⛔ | An error encountered: ${e.toString().slice(0, 1974)}`)
+        else console.error(e)
+    })
+    .on('empty', channel => channel.send({
+        embeds: [new EmbedBuilder().setColor("Red")
+            .setDescription('⛔ |Voice channel is empty! Leaving the channel...')]
+    }))
+    .on('searchNoResult', (message, query) =>
+        message.channel.send(
+            {
+                embeds: [new EmbedBuilder().setColor("Red")
+                    .setDescription('`⛔ | No result found for \`${query}\`!`')]
+            })
+    )
+    .on('finish', queue => queue.textChannel.send({
+        embeds: [new EmbedBuilder().setColor("Green")
+            .setDescription('🏁 | Queue finished!')]
+    }))
