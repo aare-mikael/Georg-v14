32647a67b1d46111a976415450b35d21151ca566
Mikael Aare <aaremikael@gmail.com>
2023-06-13 09:48:23 +0200

    plays introsound first before going back to current queue

--- Events/Guild/voiceStateUpdate.js
+++ Events/Guild/voiceStateUpdate.js
@@ -10,6 +10,7 @@ module.exports = {
   name: "voiceStateUpdate",
   async execute(oldUser, newUser) {
     if (oldUser.channel != (null || undefined)) return;
+    // TODO; "Få deg fiber Erik!"
 
     // Georg_v14
     if (newUser.id == '1089200999933169714') return;
@@ -31,7 +32,40 @@ module.exports = {
       if (user) {
         const introSound = user.introSound;
         if (introSound) {
-          await client.distube.play(voiceChannel, introSound, { leaveOnStop: true, leaveOnFinish: true });
+          
+          // Finn queue
+          const queue = await client.distube.getQueue(voiceChannel);
+
+          // Sett avspilling til loop: queue
+          let mode = await queue.getRepeatMode();
+          if (mode === 2) {
+            console.log("");
+          } else {
+            await queue.setRepeatMode(2);
+          }
+
+          // sett relevant introlyd som siste element
+          await client.distube.play(voiceChannel, introSound);
+
+          // lagre timestamp i noværande element - LATER
+          // TODO
+
+          // Finn lengden på queue, skip (queue-1)
+          const queueLength = queue.size;
+          await queue.skip(queueLength - 1);
+
+          // la queue begynne på nytt, 
+          if (mode === 2) {
+            console.log("");
+          } else {
+            setTimeout(() => {
+              queue.setRepeatMode(mode);
+            }, 30000);
+          }
+          
+          // spol til timestamp lagret fra tidligare - LATER
+          // TODO
+
         }
       }
     } catch (err) {
