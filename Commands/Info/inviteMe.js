f5ec8b061ef7130812c8454ebb8cb2c7f6ddd21c
Mikael Aare <aaremikael@gmail.com>
2023-06-13 12:29:32 +0200

    hotfix

--- Commands/Info/inviteMe.js
+++ Commands/Info/inviteMe.js
@@ -7,13 +7,16 @@ module.exports = {
     .setName("inviteMe")
     .setDescription("Get the link to add me to your server."),
 
-    execute(interaction, client) {
+    async execute(interaction) {
+
         const embed = new EmbedBuilder()
 
-        .setTitle("Invite me!")
-        .setColor("Blue")
-        .setDescription("Click [here](https://discord.com/oauth2/authorize?client_id=1089200999933169714&permissions=27895812325111&scope=bot%20applications.commands) to invite me to your server!")
+        embed
+            .setColor("Purple")
+            .setTitle("Invite me!")
+            .setDescription("Click [here](https://discord.com/oauth2/authorize?client_id=1089200999933169714&permissions=27895812325111&scope=bot%20applications.commands) to invite me to your server!")
 
-        interaction.reply({ embeds: [embed] })
+        interaction.reply({ embeds: [embed], ephemeral: true })
+        return;
     }
 }
