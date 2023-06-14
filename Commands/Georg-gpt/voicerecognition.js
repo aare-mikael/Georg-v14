c2485efd0f3f864b8a05ed5c1bb99acd0a730720
Mikael Aare <aaremikael@gmail.com>
2023-06-14 20:21:28 +0200

    Added placeholder statements

--- Commands/Georg-gpt/voicerecognition.js
+++ Commands/Georg-gpt/voicerecognition.js
@@ -1,1 +1,30 @@
 // TO DO: Spør Andreas om andre tips
+
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const georgConfig = require("../../config.json");
+const georgAPI = georgConfig.georgGPT;
+
+const georg = new Configuration({
+    apiKey: georgAPI,
+  });
+const openai = new OpenAIApi(georg);
+
+async function promptGeorg (query) {
+    const completion = await openai.createCompletion({
+    model: "gpt-3.5-turbo",
+    prompt: query,
+    max_tokens:4000
+    });
+    console.log(completion.data.choices[0].text);
+    return completion.data.choices[0].text;
+};
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("voicerecognition")
+        .setDescription("Just a placeholder for now, don't use this command"),
+    async execute(interaction) {
+        interaction.reply("This command is not ready yet, please wait for a future update.");
+    }
+};
