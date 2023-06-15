8c8c4e972cb0f97dbbcd0ed3598b34e521afdcd5
Mikael Aare <aaremikael@gmail.com>
2023-06-15 18:16:47 +0200

    Trial axios

--- Commands/Georg-gpt/georg.js
+++ Commands/Georg-gpt/georg.js
@@ -1,29 +1,33 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const { Configuration, OpenAIApi } = require("openai");
-const dotenv = require('dotenv').config();
+const axios = require("axios");
 const georgKey = process.env.georgGPT;
 
-const configuration = new Configuration({
-    apiKey: georgKey,
-  });
-const openai = new OpenAIApi(configuration);
+const axiosClient = axios.create({
+    headers: {
+        Authorization: "Bearer " + apiKey,
+    },
+});
 
 async function promptGeorg(query) {
-    const response = await openai.createCompletion({
+    
+    const params = {
+        prompt: "Say this is a test",
         model: "gpt-3.5-turbo",
-        query,
-        usage: {
-            prompt_tokens: 100,
-            completion_tokens: 800,
-            total_tokens: 1000,
-        },
-        temperature: 1,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    });
-    let answer = response.data.choices[0].text;
-    return answer;
+        max_tokens: 100,
+        temperature: 0,
+    };
+
+    axiosClient
+        .post("https://api.openai.com/v1/completions", params)
+        .then((result) => {
+            console.log(result.data.choices[0].text);
+        }).catch((err) => {
+            console.log(err);
+        });
+
+    let result = response.data.choices[0].text;
+    return result;
 };
 
 module.exports = {
