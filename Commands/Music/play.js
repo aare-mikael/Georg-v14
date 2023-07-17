5dd51c3981465c3370d0c1bf9ab6535818e88dbd
Mikael Aare <aaremikael@gmail.com>
2023-07-17 12:51:53 +0200

    Maybe work?

--- Commands/Music/play.js
+++ Commands/Music/play.js
@@ -19,25 +19,25 @@ module.exports = {
 
         const embed = new EmbedBuilder();
 
-        embed
-            .setColor("Red")
-            .setDescription(`Loading...`)
-            // .setFooter({ text: `Requested by ${member.user.tag}`, iconURL: member.displayAvatarURL() }),
-        interaction.reply({ embeds: [embed], ephemeral: false });
-
-        if (!voiceChannel) {
-            embed.setColor("Red").setDescription("You must be in a voice channel to execute music commands.");
-            interaction.editReply({ embeds: [embed], ephemeral: false });
-            return;
-        }
-
-        if (!member.voice.channelId == guild.members.me.voice.channelId) {
-            embed.setColor("Red").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
-            interaction.editReply({ embeds: [embed], ephemeral: false });
-            return;
-        }
-
         try {
+
+            embed
+                .setColor("Red")
+                .setDescription(`Loading...`)
+                .setFooter({ text: `Requested by ${member.user.tag}`, iconURL: member.displayAvatarURL() }),
+            interaction.reply({ embeds: [embed], ephemeral: false });
+
+            if (!voiceChannel) {
+                embed.setColor("Red").setDescription("You must be in a voice channel to execute music commands.");
+                interaction.editReply({ embeds: [embed], ephemeral: false });
+                return;
+            }
+
+            if (!member.voice.channelId == guild.members.me.voice.channelId) {
+                embed.setColor("Red").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
+                interaction.editReply({ embeds: [embed], ephemeral: false });
+                return;
+            }
 
             await client.distube.play(voiceChannel, query, { textChannel: channel, member: member }, { leaveOnStop: true, leaveOnEmpty: false, leaveOnFinish: true, autoPlay: false });
             embed.setColor("Green").setDescription("🎶 Request received.");
