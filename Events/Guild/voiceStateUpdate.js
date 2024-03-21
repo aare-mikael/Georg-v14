d11f6d3af1a873c32b2966410393b56c039d663d
Mikael Aare <aaremikael@gmail.com>
2024-03-21 10:20:48 +0100

    Deleted unnecessary comments

--- Events/Guild/voiceStateUpdate.js
+++ Events/Guild/voiceStateUpdate.js
@@ -10,7 +10,6 @@ module.exports = {
   name: "voiceStateUpdate",
   async execute(oldUser, newUser) {
     if (oldUser.channel != (null || undefined)) return;
-    // TODO; "Få deg fiber Erik!"
 
     // Georg_v14
     if (newUser.id == '1089200999933169714') return;
@@ -31,33 +30,7 @@ module.exports = {
       const user = await discordUsers.findOne({ id: newUser.id });
       if (user) {
         const introSound = user.introSound;
-        if (introSound) {
-          
-          // // Finn queue
-          // const queue = await client.distube.getQueue(voiceChannel);
-
-          // // Sett avspilling til loop: queue
-          // await queue.setRepeatMode(2);
-
-          // sett relevant introlyd som siste element
-          await client.distube.play(voiceChannel, introSound);
-
-          // // lagre timestamp i noværande element - LATER
-          // // TODO
-
-          // // Finn lengden på queue, skip (queue-1)
-          // const queueLength = queue.size;
-          // await queue.skip(queueLength - 1);
-
-          // // la queue begynne på nytt, og sett loop til off etter 30 sekunder
-          // setTimeout(() => {
-          //   queue.setRepeatMode(0);
-          // }, 30000);
-                    
-          // // spol til timestamp lagret fra tidligare - LATER
-          // // TODO
-
-        }
+        if (introSound) { await client.distube.play(voiceChannel, introSound) }
       }
     } catch (err) {
       console.log(err);
