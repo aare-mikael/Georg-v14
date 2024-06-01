a76ba1506fc2cd557ce6f48431b46cc84a9c77e6
Mikael Aare <aaremikael@gmail.com>
2024-06-02 00:37:30 +0200

    Added openai functionality

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const {
   Partials,
   Collection,
 } = require("discord.js");
+const { OpenAI } = require('openai');
+const georgApiKey = process.env.GEORGGPT_APIKEY;
+const georgAssistantId = process.env.GEORG_ASSISTANT_ID
 
 const dotenv = require('dotenv').config()
 const token  = process.env.TOKEN
@@ -33,6 +36,73 @@ client.commands = new Collection();
 client.config = require("./config.json");
 
 module.exports = client;
+
+const openai = new OpenAI({
+  
+})
+
+const threadMap = {};
+
+const getOpenAiThreadId = (discordThreadId) => {
+  // Temporary in-memory solution until I can scale up with a database like Firestore, Redis or DynamoDB.
+  return threadMap[discordThreadId];
+}
+
+const addThreadToMap = (discordThreadId, openAiThreadId) => {
+  threadMap[discordThreadId] = openAiThreadId;
+}
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const terminalStates = ["cancelled", "failed", "completed", "expired"];
+const statusCheckLoop = async (openAiThreadId, runId) => {
+  const run = await openai.beta.threads.runs.retrieve(
+    openAiThreadId,
+    runId
+  );
+
+  if(terminalStates.indexOf(run.status) < 0) {
+    await sleep(1000);
+    return statusCheckLoop(openAiThreadId, runId);
+  }
+
+  return run.status;
+}
+
+client.on('messageCreate', async message => {
+  if(message.author.bot) return;
+  if (message.content.toLowerCase.includes("georg")) {
+    const discordThreadId = message.channel.id;
+    let openAiThreadId = threadMap[discordThreadId];
+
+    if(!openAiThreadId) {
+      const thread = await openai.beta.threads.create();
+      openAiThreadId = thread.id;
+      addThreadToMap(discordThreadId, openAiThreadId);
+    }
+
+    await openai.beta.threads.messages,create(
+      openAiThreadId,
+      { role: "user", content: message.content }
+    )
+
+    const run = await openai.beta.threads.runs.create(
+      openAiThreadId,
+      { assistant_id: georgAssistantId }
+    );
+
+    await statusCheckLoop(openAiThreadId, run.id);
+
+    const messages = await openai.beta.threads.messages.list(openAiThreadId);
+    const response = messages.data[0].content[0].text.value;
+
+    console.log(response);
+
+    message.channel.send(response);
+  }
+})
 
 client.login(token).then(() => {
   loadEvents(client);
