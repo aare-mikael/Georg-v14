fc28d9e7414ea498829d5022a5cc4e98662f084c
Mikael Aare <aaremikael@gmail.com>
2024-06-04 21:44:48 +0200

    Removed non-functioning moderation filter

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -16,26 +16,6 @@ module.exports = {
     if (interaction.isChatInputCommand()) {
       const command = client.commands.get(commandName);
 
-      //moderation filter
-      if (command.moderatorOnly) {
-        if (member.user.id !== "227520275132973056") {
-          errEmbed.setDescription(
-            "⛔ | Whoops! You don't have permissions for that!"
-          );
-          return interaction.reply({ embeds: [errEmbed], ephemeral: false });
-        }
-      }
-
-      //admin filter
-      if (command.adminOnly) {
-        if (member.user.id !== "227520275132973056") {
-          errEmbed.setDescription(
-            "⛔ | Whoops! You don't have permissions for that!"
-          );
-          return interaction.reply({ embeds: [errEmbed], ephemeral: false });
-        }
-      }
-
       if (!command) {
         return interaction.reply({
           content: "outdated command",
